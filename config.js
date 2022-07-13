df44dfaf78e6ba11b4c80dab50d6bdc76297c98e
Javier Leguizamo <jlegui745@gmail.com>
2022-07-13 00:48:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 var fs = require('fs')
+const path = require('path')
+
+const imagePath = path.join(__dirname, '/public/img/image.png')
 
 module.exports = {
     modpack_name: 'Chimbaland 3',
@@ -8,7 +11,7 @@ module.exports = {
     branch: 'mods',
     name: 'chimba-installer',
 
-    image: 'data:image/png;base64,' + fs.readFileSync('./public/img/image.png', 'base64'),
+    image: 'data:image/png;base64,' + fs.readFileSync(imagePath, 'base64'),
     forgeVersion: '1.16.5-forge-36.2.34',
     javaArgs: '-Xmx4G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M'
 }
